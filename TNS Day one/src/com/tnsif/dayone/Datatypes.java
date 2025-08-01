513d17fb159648c2ed25a0f36e6c73706dfb8dd0
HP <HP@DESKTOP-110SKUF>
2025-08-01 13:06:11 +0530

    Day 2

---
+++ TNS Day one/src/com/tnsif/dayone/Datatypes.java
@@ -0,0 +1,23 @@
+package com.tnsif.dayone;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 20/3;
+		System.out.println(num);
+		
+		float num1 = 8/3f;
+		System.out.println(num1);
+		
+		double num2 = 8/3f;
+		System.out.println(num2);
+		
+		char s = 'A';
+		System.out.println(s);
+		
+		boolean result = false;
+		System.out.println(result);
+	}
+
+}
---
+++ TNS Day one/src/com/tnsif/dayone/VariableDay2.java
@@ -0,0 +1,21 @@
+package com.tnsif.dayone;
+
+public class VariableDay2 {
+
+	public static void main(String[] args) {
+		// Varibles declaration - Invalid Indentifier 
+		//int for = 20; Keywords are not allowed for variable declaration
+		//int  num 1 = 0; No space is allowed 
+		//int @num = 0; 
+		
+		//Valid identifiers
+		int num = 0;
+		int $a = 20;
+		int Roll_Number = 8;
+		System.out.println("Integer values : "+$a+" "+Roll_Number+" "+num);
+	
+		String fullName = "Asmita";
+		System.out.println(fullName);
+		
+	}
+}
